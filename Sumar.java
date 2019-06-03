4ae5b2da7d8cad8a6ff73bf09dc7b6ac41f11b49
Nelson <nelsonortiz2712@gmail.com>
2019-06-03 17:40:48 -0500

    Iniciando

---
+++ Sumar.java
@@ -0,0 +1,33 @@
+package Paquete1;
+
+public class Sumar 
+{
+	private int a;
+	private int b;
+	
+	public Sumar(){}
+
+	public int hacersumar(int a, int b)
+	{
+		return a + b;
+		
+	}
+	
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+	
+	
+}
---
+++ prueba.java
@@ -0,0 +1,12 @@
+package Paquete1;
+
+public class prueba 
+{
+	public static void main (String [] args)
+	{
+		Sumar sumar = new Sumar();
+		System.out.println(sumar.hacersumar(12, 2));
+		
+	}
+	
+}
