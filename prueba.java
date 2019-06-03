1503093cea2562681a68c297e80165b92460ed92
Nelson <nelsonortiz2712@gmail.com>
2019-06-03 17:47:19 -0500

    commit 3 se agrego metodo restar

--- prueba.java
+++ prueba.java
@@ -5,7 +5,9 @@ public class prueba
 	public static void main (String [] args)
 	{
 		Sumar sumar = new Sumar();
-		System.out.println(sumar.hacersumar(12, 2));
+		System.out.println("Sumar ="+sumar.hacersumar(12, 2));
+		
+		System.out.println("Resar ="+sumar.restar(12, 2));
 		
 	}
 	
