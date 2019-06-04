4eea4997b5d9974b49a21d10a567d1a8ae0ac9f7
Nelson <nelsonortiz2712@gmail.com>
2019-06-03 19:05:52 -0500

    creando clase Lbro

---
+++ Lbro.java
@@ -0,0 +1,20 @@
+package Paquete1;
+
+public class Lbro 
+{
+	private String nombre;
+	private String precio;
+	
+	public Lbro(){}
+	
+	private String getNombre()
+	{
+		
+		return nombre;
+	}
+	private void setNombre(String nombre)
+	{
+		this.nombre = nombre;
+	}
+	
+}
